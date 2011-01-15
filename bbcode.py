4bb15e01fe6ca0bb995777121b2d205137a7fe97
Jihyun Yu <yjh0502@gmail.com>
2011-01-16 02:50:25 +0900

    modify bbcode: now support list tag and video tag

--- bbcode.py
+++ bbcode.py
@@ -3,17 +3,52 @@
 
 import re
 
-_tags = {"b": {"argc":1, "tmpl":"<b>%s</b>", "nest":True},
-		"i": {"argc":1, "tmpl":"<i>%s</i>", "nest":True},
-		"u": {"argc":1, "tmpl":"<u>%s</u>", "nest":True},
-		"s": {"argc":1, "tmpl":"<s>%s</s>", "nest":True},
-		"center": {"argc":1, "tmpl":"<center>%s</center>", "nest":True},
+_re_url = re.compile(r"((https?):((//)|(\\\\))+[\w\d:#@%/;$()~_?\+-=\\\.&]*)", re.MULTILINE|re.UNICODE)
+_re_list = re.compile('\[\*\]([^[]*)')
 
-		"link": {"argc":2, "tmpl":"<a href=\"%s\">%s</a>", "nest":True},
-		"img": {"argc":2, "tmpl":"<img src=\"%s\" alt=\"%s\"/>", "nest":False},
-		"color": {"argc":2, "tmpl":"<span style=\"color:%s;\">%s</span>", "nest":True},
-		"code": {"argc":2, "tmpl":"<pre type=\"%s\">%s</pre>", "nest":False},
-		"quote": {"argc":2, "tmpl":"<b>%s</b><br/><blockquote>%s</blockquote>", "nest":True},
+def _fmt(tmpl):
+	if(tmpl.count("%s") == 1):
+		return lambda args: tmpl % args[1];
+	return lambda args: tmpl % args;
+
+def _fmt_video(args):
+	width = 640
+	height = 480
+	url = ''
+	if(args[0] == "youtube"):
+		url = '<iframe title="YouTube video player" class="youtube-player" type="text/html" width="%s" height="%s" src="http://www.youtube.com/embed/%s?rel=0" frameborder="0"></iframe>' % (width, height, args[1])
+	elif(args[0] == "vimeo"):
+		url = '<iframe width="%s" height="%s" src="http://player.vimeo.com/video/%s" frameborder="0"></iframe>' % (width, height, args[1])
+	return url
+
+def _fmt_list(args):
+	style = "list-style-type:circle"
+	if args[0] == "1":
+		style = 'list-style-type:decimal'
+	elif args[0] == 'a':
+		style = 'list-style-type:lower-alpha'
+	elif args[0] == 'A':
+		style = 'list-style-type:upper-alpha'
+	
+	html = '<ol style="%s">'
+	for elem in _re_list.findall(args[1]):
+		html += '<li>' + _parse(elem, _tags) + '</li>'
+	html += '</ol>'
+	return html
+
+_tags = {"b": {"tmpl":_fmt("<b>%s</b>"), "nest":True},
+		"i": {"tmpl":_fmt("<i>%s</i>"), "nest":True},
+		"u": {"tmpl":_fmt("<u>%s</u>"), "nest":True},
+		"s": {"tmpl":_fmt("<s>%s</s>"), "nest":True},
+		"center": {"tmpl":_fmt("<center>%s</center>"), "nest":True},
+
+		"link": {"tmpl":_fmt("<a href=\"%s\">%s</a>"), "nest":True},
+		"img": {"tmpl":_fmt("<img src=\"%s\" alt=\"%s\"/>"), "nest":False},
+		"color": {"tmpl":_fmt("<span style=\"color:%s;\">%s</span>"), "nest":True},
+		"code": {"tmpl":_fmt("<pre type=\"%s\">%s</pre>"), "nest":False},
+		"quote": {"tmpl":_fmt("<b>%s</b><br/><blockquote>%s</blockquote>"), "nest":True},
+		"video": {"tmpl":_fmt_video, "nest":False},
+		"list": {"tmpl":_fmt_list, "nest":False},
 		};
 
 def getRegexStart(tags):
@@ -23,17 +58,16 @@ def getRegexStart(tags):
 	re_text = re_text[:len(re_text)-1] + ')=?([^\]]*)]'
 	return re.compile(re_text)
 
-_re_url = re.compile(r"((https?):((//)|(\\\\))+[\w\d:#@%/;$()~_?\+-=\\\.&]*)", re.MULTILINE|re.UNICODE)
 _re_start = getRegexStart(_tags);
 
-def _parse_text(text):
+def _parse_url(text):
 	html = ''
 	while True:
 		ro = _re_url.search(text)
 		if(ro == None):
 			break
 		html, text, url = html + text[:ro.start()], text[ro.end():], ro.groups()[0]
-		html += _tags["link"]["tmpl"] % (url, url)
+		html += _tags["link"]["tmpl"]((url, url))
 	return html + text
 
 def _parse(text, tags):
@@ -45,18 +79,15 @@ def _parse(text, tags):
 		tag, arg = ro.groups()[0], ro.groups()[1]
 		if arg == None:
 			arg = ''
-		html, text = html + _parse_text(text[:ro.start()]), text[ro.end():]
+		html, text = html + _parse_url(text[:ro.start()]), text[ro.end():]
 		innerText, text = text.split('[/%s]' % tag, 1)
 		if tags[tag]["nest"]:
 			innerHtml = _parse(innerText, tags)
 		else:
 			innerHtml = innerText
 
-		if tags[tag]["argc"] == 1:
-			html += tags[tag]["tmpl"] % innerHtml
-		else:
-			html += tags[tag]["tmpl"] % (arg, innerHtml)
-	return html + _parse_text(text)
+		html += tags[tag]["tmpl"]((arg, innerHtml))
+	return html + _parse_url(text)
 
 def parse(text):
 	text = text.replace('\n', '<br/>')
