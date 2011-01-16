3802fd057c79caf6f4f4701a62fb68de7e4cccff
Park Shinjo <peremen@gmail.com>
2011-01-16 14:19:30 +0900

    기존 템플릿은 classic으로 빼내고 새 디자인 준비.

--- render.py
+++ render.py
@@ -8,12 +8,18 @@ desktop_render = render_mako(
     input_encoding = 'utf-8', output_encoding = 'utf-8',
 )
 
+classic_render = render_mako(
+    directories = [os.path.join(os.path.dirname(__file__), 'templates/classic/').replace('\\','/'),],
+    input_encoding = 'utf-8', output_encoding = 'utf-8',
+)
+
 mobile_render = render_mako(
     directories = [os.path.join(os.path.dirname(__file__), 'templates/mobile/').replace('\\','/'),],
     input_encoding = 'utf-8', output_encoding = 'utf-8',
 )
 render = {
         'default': desktop_render, 
+        'classic': classic_render, 
         'm': mobile_render,
-        }
+}
 
